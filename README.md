30e6295d0aae1de296457d74ae0e116dc1a25ed3
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-06-27 00:31:40 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TTGO T8 V1.1
+
+TTGO T8
+ESP32, 4 MB Flash, 4 MB PS-RAM, 3D Antenna, SD Card Slot, Li-Po Charger
+เป็นบอร์ด ESP32 ขนาดหน่วยความจำ 4 MB Flash Memory, 4 MB PS-RAM, SD Card Reader และวงจรชารจ์ Li-Po Battery
+
+สั่งซื้อ : https://thai-maker.com/product/ttgo-t8/
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "ttgo-t8-v1.3",
+  platform: "arduino-esp32",
+  title: "TTGO T8 V1.3",
+  description: "TTGO T8 ESP32, 4 MB Flash, 4 MB PS-RAM, 3D Antenna, SD Card Slot, Li-Po Charger",
+  author: "Maker Asia",
+  website: "",
+  email: "makerasia@gmail.com",
+  git: "https://github.com/cmmakerclub/ttgo-t8-v1.3-board/",
+  image: "/static/display.jpg",
+  version: "1.0.0",
+};

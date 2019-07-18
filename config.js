d75494d8c4b0ba1843dc522950eb5c3678c23609
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-07-18 15:16:02 +0700

    config.js : add vender

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: "ttgo-t8-v1.3",
+  vender: "LilyGO Xin Yuan Electronic Technology Co., Ltd",
   platform: "arduino-esp32",
   title: "TTGO T8 V1.3",
   description: "TTGO T8 ESP32, 4 MB Flash, 4 MB PS-RAM, 3D Antenna, SD Card Slot, Li-Po Charger",
@@ -8,5 +9,5 @@ module.exports = {
   email: "makerasia@gmail.com",
   git: "https://github.com/cmmakerclub/ttgo-t8-v1.3-board/",
   image: "/static/display.jpg",
-  version: "1.0.0",
+  version: "1.0.1",
 };
